7595ee1b8328d827a4238bf43bdcdfe6fe62c4d8
Asadbek <ibnnumon@gmail.com>
2023-12-25 21:27:39 +0500

    [NEW] hex color to rgb color

--- hexToRGB.js
+++ hexToRGB.js
@@ -1,3 +1,5 @@
+// source: https://github.com/nuxt-ui-pro
+
 export function hexToRgb(hex) {
   const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
   hex = hex.replace(shorthandRegex, function(_, r, g, b) {
@@ -5,5 +7,6 @@ export function hexToRgb(hex) {
   });
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result ? `${parseInt(result[1], 16)} ${parseInt(result[2], 16)} ${parseInt(result[3], 16)}` : null;
-}
+} 
+
 
