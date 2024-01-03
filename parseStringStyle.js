86b3d062ab4f6a01ea55500c37120c0a7a0d700d
asad <87940040+Acadbek@users.noreply.github.com>
2024-01-03 22:53:06 +0500

    Create parseStringStyle.js

---
+++ parseStringStyle.js
@@ -0,0 +1,15 @@
+// Source: vue-router.js
+
+var listDelimiterRE = /;(?![^(]*\))/g;
+var propertyDelimiterRE = /:([^]+)/;
+var styleCommentRE = /\/\*[^]*?\*\//g;
+function parseStringStyle(cssText) {
+  const ret = {};
+  cssText.replace(styleCommentRE, "").split(listDelimiterRE).forEach((item) => {
+    if (item) {
+      const tmp = item.split(propertyDelimiterRE);
+      tmp.length > 1 && (ret[tmp[0].trim()] = tmp[1].trim());
+    }
+  });
+  return ret;
+}
